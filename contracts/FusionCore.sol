38cc1cd495fadb9d4473e29a4d8f51f72cf708c5
John <jooohn.eth@gmail.com>
2022-09-05 13:55:04 -0400

    Imports, mappings, token init

--- contracts/FusionCore.sol
+++ contracts/FusionCore.sol
@@ -1,6 +1,21 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "./FusionToken.sol";
+
 contract FusionCore {
+
+    IERC20 public usdcToken;
+    FusionToken public fusionToken;
+
+    mapping(address => uint) public lendingBalance;
+    mapping(address => uint) public fusionBalance;
+    mapping(address => uint) public startTime;
+    mapping(address => bool) public isStaking;
     
+    constructor(IERC20 _usdcAddress, FusionToken _fusionAddress) {
+        usdcToken = _usdcAddress;
+        fusionToken = _fusionAddress;
+    } 
 }
