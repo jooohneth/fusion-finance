43b0ae7a6f926467163d57089116051dd3435a8c
John <jooohn.eth@gmail.com>
2022-09-05 13:35:02 -0400

    Setup ERC20 token and wrapped _mint function

--- contracts/FusionToken.sol
+++ contracts/FusionToken.sol
@@ -1,6 +1,15 @@
 //SPDX-License-Identifier: MIT
 pragma solidity ^0.8.9;
 
-contract FusionToken {
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract FusionToken is ERC20, Ownable {
+
+    constructor() ERC20("Fusion Token", "FUSN") {}
+
+    function mint(address _to, uint _amount) public onlyOwner {
+        _mint(_to, _amount);
+    }
 
 }
