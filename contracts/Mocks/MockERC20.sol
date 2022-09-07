f53c3669ced501791b816bed6da140f71b386dda
John <jooohn.eth@gmail.com>
2022-09-07 10:25:41 -0400

    Created MockERC20 for testing

---
+++ contracts/Mocks/MockERC20.sol
@@ -0,0 +1,16 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MockERC20 is ERC20 {
+
+    constructor(
+        string memory name,
+        string memory ticker
+    ) ERC20(name, ticker) {}
+
+    function mint(address to, uint256 amount) public {
+        _mint(to, amount);
+    }
+}
