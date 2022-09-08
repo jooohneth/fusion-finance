526fd0823a1ba0708376f0d68a457137e184a922
John <jooohn.eth@gmail.com>
2022-09-08 16:57:35 -0400

    Added borrow and repay functionality, minor changes

--- contracts/FusionCore.sol
+++ contracts/FusionCore.sol
@@ -8,11 +8,13 @@ import "./FusionToken.sol";
 contract FusionCore {
 
     ///@notice staking events emitted after each action.
-    event Lend(address indexed sender, uint amount);
-    event Withdraw(address indexed sender, uint amount);
-    event ClaimYield(address indexed sender, uint amount);
-    event Collateralize(address indexed sender, uint amount);
-    event WithdrawCollateral(address indexed sender, uint amount);
+    event Lend(address indexed lender, uint amount);
+    event WithdrawLend(address indexed lender, uint amount);
+    event ClaimYield(address indexed lender, uint amount);
+    event Collateralize(address indexed borrower, uint amount);
+    event WithdrawCollateral(address indexed borrower, uint amount);
+    event Borrow(address indexed borrower, uint amount);
+    event Repay(address indexed borrower, uint amount);
 
     ///@notice mappings needed to keep track of lending
     mapping(address => uint) public lendingBalance;
@@ -20,9 +22,11 @@ contract FusionCore {
     mapping(address => uint) public startTime;
     mapping(address => bool) public isStaking;
 
-    ///@notice mappings needed to keep track of collateral
+    ///@notice mappings needed to keep track of collateral and borrowing
     mapping(address => uint) public collateralBalance;
+    mapping(address => uint) public borrowBalance;
     mapping(address => uint) public borrowLimit;
+    mapping(address => bool) public isBorrowing;
 
     ///@notice declaring chainlink's price aggregator.
     AggregatorV3Interface internal priceFeed;
@@ -60,10 +64,10 @@ contract FusionCore {
 
     ///@notice calculates the borrow limit depending on the price of ETH and borrow limit rate.
     ///@return limit current borrow limit for user
-    function calculateBorrowLimit() public view returns(uint limit) {
+    function calculateBorrowLimit(address _lender) public view returns(uint limit) {
         (,int price,,,) = priceFeed.latestRoundData();
         uint ethPrice = uint(price) / 10**8;
-        limit = (((uint(ethPrice) * collateralBalance[msg.sender]) / 100) * 70) / 10**18;
+        limit = (((ethPrice * collateralBalance[_lender]) / 100) * 70) / 10**18;
     }
 
     ///@notice lends usdc.
@@ -81,7 +85,7 @@ contract FusionCore {
         startTime[msg.sender] = block.timestamp;
         isStaking[msg.sender] = true;
 
-        usdcToken.transferFrom(msg.sender, address(this), _amount);
+        require(usdcToken.transferFrom(msg.sender, address(this), _amount), "Transaction failed!");
 
         emit Lend(msg.sender, _amount);
     }
@@ -98,14 +102,14 @@ contract FusionCore {
         _amount = 0;
         lendingBalance[msg.sender] -= withdrawAmount;
 
-        usdcToken.transfer(msg.sender, withdrawAmount);
+        require(usdcToken.transfer(msg.sender, withdrawAmount), "Transaction failed!");
         fusionBalance[msg.sender] += yield;
 
         if(lendingBalance[msg.sender] == 0){
             isStaking[msg.sender] = false;
         }
 
-        emit Withdraw(msg.sender, withdrawAmount);
+        emit WithdrawLend(msg.sender, withdrawAmount);
     }
     
     ///@notice claims all yield earned by lender.
@@ -131,7 +135,12 @@ contract FusionCore {
         require(msg.value > 0, "Can't collaterlize ETH amount: 0!");
 
         collateralBalance[msg.sender] += msg.value;
-        borrowLimit[msg.sender] = calculateBorrowLimit();
+
+        if(isBorrowing[msg.sender]){
+            borrowLimit[msg.sender] = calculateBorrowLimit(msg.sender) - borrowBalance[msg.sender];
+        } else {
+            borrowLimit[msg.sender] = calculateBorrowLimit(msg.sender);
+        }
 
         emit Collateralize(msg.sender, msg.value);
     } 
@@ -140,13 +149,48 @@ contract FusionCore {
     ///@param _amount amount of ETH the user wants to withdraw
     function withdrawCollateral(uint _amount) public {
         require(collateralBalance[msg.sender] >= _amount, "Not enough collateral to withdraw!");
+        require(!isBorrowing[msg.sender], "Can't withdraw collateral while borrowing!");
 
         collateralBalance[msg.sender] -= _amount;
-        borrowLimit[msg.sender] = calculateBorrowLimit();
+        borrowLimit[msg.sender] = calculateBorrowLimit(msg.sender);
 
         (bool success, ) = msg.sender.call{value: _amount}("");
         require(success, "Transaction Failed!");
 
         emit WithdrawCollateral(msg.sender, _amount);
+    }
+
+    ///@notice borrows usdc
+    ///@param _amount amount of usdc to borrow
+    function borrow(uint _amount) public {
+        require(collateralBalance[msg.sender] > 0, "No ETH collateralized!");
+        require(borrowLimit[msg.sender] >= _amount, "Borrow amount exceeds borrow limit!");
+
+        isBorrowing[msg.sender] = true;
+        borrowBalance[msg.sender] += _amount;
+        borrowLimit[msg.sender] -= _amount;
+
+        require(usdcToken.transfer(msg.sender, _amount), "Transaction failed!");
+
+        emit Borrow(msg.sender, _amount);
+    }
+    
+    ///@notice repays usdc debt
+    ///@param _amount amount of usdc to repay
+    function repay(uint _amount) public {
+        require(isBorrowing[msg.sender], "Can't repay before borrowing!");
+        require(usdcToken.balanceOf(msg.sender) >= _amount, "Insufficient funds!");
+        require(_amount > 0 && _amount <= borrowBalance[msg.sender], "Can't repay amount: 0 or more than amount borrowed!");
+
+        if(_amount == borrowBalance[msg.sender]){ 
+            isBorrowing[msg.sender] = false;
+        }
+
+        borrowLimit[msg.sender] += _amount;
+        borrowBalance[msg.sender] -= _amount;
+
+        require(usdcToken.transferFrom(msg.sender, address(this), _amount), "Transaction Failed!");
+
+        emit Repay(msg.sender, _amount);
     }
 }
