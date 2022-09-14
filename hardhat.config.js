df3332def7c64113105d1b91dba424d8c2311b13
John <jooohn.eth@gmail.com>
2022-09-14 17:36:02 -0400

    Setting up goerli fork for testing price feed

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,6 +1,14 @@
 require("@nomicfoundation/hardhat-toolbox");
+require("dotenv").config();
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
+  networks: {
+    hardhat: {
+      forking: {
+        url: process.env.ALCHEMY_GOERLI_RPC_URL,
+      },
+    },
+  },
   solidity: "0.8.9",
 };
