972e67e497906d09a6aff1b195baceb0aba5452f
John <jooohn.eth@gmail.com>
2022-09-15 17:56:23 -0400

    Minor changes: added error message(in passedLiquidation modifier)

--- contracts/FusionCore.sol
+++ contracts/FusionCore.sol
@@ -48,7 +48,7 @@ contract FusionCore {
     ///@notice checks if the borrow position has passed the liquidation point
     modifier passedLiquidation(address _borrower) {
         uint ethPrice = getEthPrice();
-        require((ethPrice * collateralBalance[_borrower]) <= calculateLiquidationPoint(_borrower));
+        require((ethPrice * collateralBalance[_borrower]) <= calculateLiquidationPoint(_borrower), "Position can't be liquidated!");
         _;
     }
 
