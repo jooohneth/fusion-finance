bdec20d947f5fac7ff22f850f4dd1aa64947ea1a
John <jooohn.eth@gmail.com>
2022-09-16 15:44:52 -0400

    Added networks, api keys

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,14 +1,33 @@
 require("@nomicfoundation/hardhat-toolbox");
 require("dotenv").config();
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   networks: {
     hardhat: {
       forking: {
-        url: process.env.ALCHEMY_GOERLI_RPC_URL,
+        url: process.env.GOERLI_FORK_RPC,
+      },
+      chainId: 1337,
+    },
+    goerli: {
+      url: process.env.GOERLI_RPC,
+      gasPrice: 50000000000,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 5,
+    },
+  },
+  etherscan: {
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+  solidity: {
+    version: "0.8.9",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200,
       },
     },
   },
-  solidity: "0.8.9",
 };
