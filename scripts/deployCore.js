27749614f4b28efac83a7c090799b7f93ff230d0
John <jooohn.eth@gmail.com>
2022-09-17 11:44:08 -0400

    Changes base asset address, netowrk detect logic

--- scripts/deployCore.js
+++ scripts/deployCore.js
@@ -1,16 +1,17 @@
 const { ethers, network } = require("hardhat");
 
 const main = async () => {
-  console.log(`Deploying on ${network.name}`);
+  let baseAssetAddress;
 
-  let baseAsset;
-
-  ///@dev uncomment the code below to deploy MOCK on Hardhat network.
-  /*
-  const MockERC20 = await ethers.getContractFactory("MockERC20");
-  baseAsset = await MockERC20.deploy("Mock Asset", "MOCK");
-  console.log(`Base asset mock address: ${baseAsset.address}`);
-  */
+  if (network.name === "hardhat") {
+    const MockERC20 = await ethers.getContractFactory("MockERC20");
+    let baseAsset = await MockERC20.deploy("Mock Asset", "MOCK");
+    baseAssetAddress = baseAsset.address;
+    console.log(`Base asset mock address: ${baseAssetAddress}`);
+  } else if (network.name === "goerli") {
+    baseAssetAddress = "0x11fE4B6AE13d2a6055C8D9cF65c55bac32B5d844";
+    console.log(`Base asset address: ${baseAssetAddress}`);
+  }
 
   const FusionToken = await ethers.getContractFactory("FusionToken");
   const fusionToken = await FusionToken.deploy();
@@ -18,14 +19,11 @@ const main = async () => {
     `Fusion Token  address: ${fusionToken.address}, deployer: ${fusionToken.signer.address}`
   );
 
-  if (!baseAsset) {
-    baseAsset = "0xD87Ba7A50B2E7E660f678A895E4B72E7CB4CCd9C";
-  } else {
-    baseAsset = baseAsset.address;
-  }
-
   const FusionCore = await ethers.getContractFactory("FusionCore");
-  const fusionCore = await FusionCore.deploy(baseAsset, fusionToken.address);
+  const fusionCore = await FusionCore.deploy(
+    baseAssetAddress,
+    fusionToken.address
+  );
   console.log(
     `Fusion Core address: ${fusionCore.address}, deployer: ${fusionCore.signer.address}`
   );
