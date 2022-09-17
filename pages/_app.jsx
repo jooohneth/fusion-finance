82cb07f5abb9aa990307a80b0cd105c9316474d8
John <jooohn.eth@gmail.com>
2022-09-17 00:52:00 -0400

    Added Navbar to pages: Issue #17

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,11 +1,24 @@
 import "../styles/globals.css";
 import { MoralisProvider } from "react-moralis";
+import Navbar from "../components/Navbar.jsx";
+import Head from "next/head";
 
 function MyApp({ Component, pageProps }) {
   return (
-    <MoralisProvider initializeOnMount={false}>
-      <Component {...pageProps} />
-    </MoralisProvider>
+    <>
+      <Head>
+        <title>Fusion Finance</title>
+        <meta
+          name="description"
+          content="A decentralized DeFi protocol for lending and borrowing"
+        />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <MoralisProvider initializeOnMount={false}>
+        <Navbar />
+        <Component {...pageProps} />
+      </MoralisProvider>
+    </>
   );
 }
 
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,16 +1,6 @@
-import Head from "next/head";
-
 export default function Home() {
   return (
     <div>
-      <Head>
-        <title>Fusion Finance</title>
-        <meta
-          name="description"
-          content="A decentralized DeFi protocol for lending and borrowing"
-        />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
       <h1>Home Page</h1>
     </div>
   );
