12057fe2195e4c2c49c763d95be5094bdf1e8caf
John <jooohn.eth@gmail.com>
2022-09-22 19:15:58 -0400

    Added polygon rpc and polygonscan key

--- hardhat.config.js
+++ hardhat.config.js
@@ -6,7 +6,9 @@ module.exports = {
   networks: {
     hardhat: {
       forking: {
+        //Comment the first line and uncomment the second line to test on polygon mainnet
         url: process.env.GOERLI_RPC,
+        //url: process.env.POLYGON_RPC,
       },
       chainId: 1337,
     },
@@ -16,9 +18,17 @@ module.exports = {
       accounts: [process.env.PRIVATE_KEY],
       chainId: 5,
     },
+    polygon: {
+      url: process.env.POLYGON_RPC,
+      accounts: [process.env.PRIVATE_KEY],
+      chainId: 137,
+    },
   },
   etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
+    apiKey: {
+      goerli: process.env.ETHERSCAN_API_KEY,
+      polygon: process.env.POLYGONSCAN_API_KEY,
+    },
   },
   solidity: {
     version: "0.8.9",
